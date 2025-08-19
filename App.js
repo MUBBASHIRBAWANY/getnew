23906d405386524ed36d0ac80e96786cf0ae2f26
MUBBASHIRBAWANY <mubbashirbawany@gmail.com>
2025-08-19 06:12:24 -0700

    2 week ago3

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import BenifichryRouter from "./Routes/BenifichryRouter.js"
 import UserRoleRouter from "./Routes/userRoleRoute.js"
 
 
+
 const Router = express.Router()
 const app = express()
 app.use(cors());
@@ -17,6 +18,6 @@ dataBase()
 export const users = app.use('/users', UserRouter)
 export const DepartmentRoute = app.use('/dep', DeparmentRouter)
 export const BenifichryRoute = app.use('/Benifichry', BenifichryRouter)
-export const UserRoleRoute  = app.use("/userRole" , UserRoleRouter)
+export const UserRoleRoute = app.use("/userRole", UserRoleRouter)
 
 
