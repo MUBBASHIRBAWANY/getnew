47d579a799bb57f5d3e1dd40501c2c0980840422
MUBBASHIRBAWANY <mubbashirbawany@gmail.com>
2025-08-19 06:36:42 -0700

    2 week ago3

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const app = express();
 const port = process.env.PORT
 app.use(cors())
 app.use(express.json())
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 console.log(port)
