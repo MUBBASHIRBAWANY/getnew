2d2e7726cca83daa2db146b75111d9134412ca8a
MUBBASHIRBAWANY <mubbashirbawany@gmail.com>
2025-08-20 05:23:05 -0700

    2 week ago2

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,14 +1,11 @@
 import express from "express";
 import { deleteUser, getUserById, getUsers, updateUser, userLogin, userProfile, userRegister } from "../Controllar/userControllar.js";
-import uploads from "../Midelware/CloudNaryHelper.js";
 const app = express()
 const UserRouter = express.Router()
 
 
 
-UserRouter.post('/', uploads.fields([
-    {name: 'image', maxCount: 1},
-]),userRegister)
+UserRouter.post('/',userRegister)
 
 UserRouter.post('/Login', userLogin)
 UserRouter.post('/profile', userProfile)
